a1ef6b531503e484649a7228814d9c9754166d3c
leshem321 <46571827+leshem321@users.noreply.github.com>
2020-08-02 19:26:36 +0300

    Changed zise
    changed player size and made that if you fall you will come back up

--- Assets/Project/scripts/Gameplay/Player/Teleport.cs
+++ Assets/Project/scripts/Gameplay/Player/Teleport.cs
@@ -16,6 +16,7 @@ public class Teleport : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        //mousePos.z = Camera.main.nearClipPlane;
         if (Input.GetKeyDown(KeyCode.T))
             loc = transform.position;
         if (Input.GetKeyDown(KeyCode.Y))
@@ -64,5 +65,47 @@ public class Teleport : MonoBehaviour
         mid.y = (from.y + to.y) / 2;
         mid.z = 0;
         return mid;
+    }
+
+    public float dist(Vector3 p1, Vector3 p2)
+    {
+        float sum;
+        sum = Mathf.Sqrt(Mathf.Pow(p1.x - p2.x, 2) + Mathf.Pow(p1.y - p2.y, 2));
+        return sum;
+    }
+
+    /*
+    public Vector3[] tp(Vector3[] arr)
+    {
+        float dis = float.MaxValue;
+        while (dis % 10 == 0)
+        {
+            dis = dist(arr[0], arr[1]);
+            Vector3[] narr = new Vector3[arr.Length * 2];
+            for (int i = 0; i < arr.Length; i++)
+            {
+                narr[0] = arr[0];
+                narr[narr.Length - 1] = arr[arr.Length - 1];
+                for (int j = 1; i < narr.Length * 2 - 1; j++)
+                {
+                    narr[j + 1] = arr[j];
+                }
+                if (i + 1 <= arr.Length)
+                {
+                    for (int j = 1; j < narr.Length-1; j += 2)
+                    {
+                        narr[j] = MidCalc(arr[])
+                    }
+                }
+            }
+        }
+    }
+    */
+
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.name == "Death")
+            teleport();
     }
 }

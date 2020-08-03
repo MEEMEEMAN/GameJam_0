46792fc0a33b21dbd5f0d2a1f0aa8c1e77b3aa44
leshem321 <46571827+leshem321@users.noreply.github.com>
2020-08-03 17:13:28 +0300

    Shuriken stuck on objects

---
+++ Assets/Project/scripts/Gameplay/Weapons/aShuriken.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class aShuriken : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        Debug.Log(collision.gameObject.tag);
+        if (collision.gameObject.tag != "Ground")
+        {
+            transform.SetParent(collision.gameObject.transform, true);
+            GetComponent<Rigidbody2D>().bodyType = RigidbodyType2D.Kinematic;
+        }
+        GetComponent<Rigidbody2D>().constraints = RigidbodyConstraints2D.FreezeAll;
+    }
+    //private void OnCollisionExit2D(Collision2D collision)
+    //{
+        //Destroy(gameObject);
+    //}
+}
